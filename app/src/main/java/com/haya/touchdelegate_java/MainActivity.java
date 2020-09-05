a7c34b21ad05d9c8994e062adff7622df6a58063
hayamizu_shinji <hayamizu.hnut@gmail.com>
2020-09-05 17:50:09 +0900

    add

---
+++ app/src/main/java/com/haya/touchdelegate_java/MainActivity.java
@@ -0,0 +1,67 @@
+//https://developer.android.google.cn/training/gestures/viewgroup?hl=ja
+
+package com.haya.touchdelegate_java;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Rect;
+import android.os.Bundle;
+import android.view.TouchDelegate;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // Get the parent view
+        View parentView = findViewById(R.id.parent_layout);
+
+        parentView.post(new Runnable() {
+            // Post in the parent's message queue to make sure the parent
+            // lays out its children before you call getHitRect()
+            @Override
+            public void run() {
+                // The bounds for the delegate view (an ImageButton
+                // in this example)
+                Rect delegateArea = new Rect();
+                Button myButton = (Button) findViewById(R.id.button);
+                myButton.setEnabled(true);
+                myButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Toast.makeText(MainActivity.this,
+                                "Touch occurred within ImageButton touch region.",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+                // The hit rectangle for the ImageButton
+                myButton.getHitRect(delegateArea);
+
+                // Extend the touch area of the ImageButton beyond its bounds
+                // on the right and bottom.
+                delegateArea.bottom += 100;
+                delegateArea.left += 100;
+
+                // Instantiate a TouchDelegate.
+                // "delegateArea" is the bounds in local coordinates of
+                // the containing view to be mapped to the delegate view.
+                // "myButton" is the child view that should receive motion
+                // events.
+                TouchDelegate touchDelegate = new TouchDelegate(delegateArea,
+                        myButton);
+
+                // Sets the TouchDelegate on the parent view, such that touches
+                // within the touch delegate bounds are routed to the child.
+                if (View.class.isInstance(myButton.getParent())) {
+                    ((View) myButton.getParent()).setTouchDelegate(touchDelegate);
+                }
+            }
+        });
+    }
+}
